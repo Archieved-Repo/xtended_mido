20446de752f395e6296b443590e08467fc0d9c9f
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2021-12-24 02:34:36 +0530

    mido: Add support for Google Recorder
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- xtended_mido.mk
+++ xtended_mido.mk
@@ -34,6 +34,7 @@ XTENDED_BUILD_DONATE_URL := https://paypal.me/pratyakshb5495
 TARGET_GAPPS_ARCH := arm64
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SCREEN_DENSITY := 400
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
